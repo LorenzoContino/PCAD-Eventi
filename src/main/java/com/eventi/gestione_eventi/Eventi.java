8cf23b0f9f3f24b1dee688855fb8c2da0e054048
Lorenzo Contino <contino424@gmail.com>
2023-05-03 11:10:07 +0200

    first commit

---
+++ src/main/java/com/eventi/gestione_eventi/Eventi.java
@@ -0,0 +1,71 @@
+package com.eventi.gestione_eventi;
+
+import java.util.TreeMap;
+
+import java.util.Map;
+
+
+public  class Eventi {    
+    private Map<String,Evento> eventList;
+
+    public Eventi() {
+        this.eventList = new TreeMap<>();
+    }
+
+    public void crea(String name, Integer seats) {
+        if (this.containsEvent(name))
+            throw new IllegalArgumentException("L'evento che si intende creare è già esistente");
+        
+        eventList.put(name,new Evento(name, seats));
+    }
+
+
+
+    public void aggiungi(String name, Integer seats) {
+       
+        if(!this.containsEvent(name))
+            throw new IllegalArgumentException("Evento non esistente");
+        eventList.get(name).addSeats(seats);
+
+    }
+
+    public void prenota(String name, Integer seats) {
+        
+       
+        if(!this.containsEvent(name))
+            throw new IllegalArgumentException("Evento non esistente");
+        eventList.get(name).removeSeats(seats);
+
+    }
+
+
+
+
+    public void listaEventi() {
+        for (Evento evento : eventList.values()) {
+            System.out.println( evento.toString());
+           
+        }
+    }
+
+
+
+
+
+    public void chiudi(String name) {
+        
+        if(!this.containsEvent(name))
+            throw new IllegalArgumentException("L'evento che si intende chiudere non è esistente");
+    
+        eventList.remove(name);
+        
+       
+}
+
+
+
+
+    private boolean containsEvent(String name) {
+        return this.eventList.containsKey(name);
+    }
+}
---
+++ src/main/java/com/eventi/gestione_eventi/Evento.java
@@ -0,0 +1,56 @@
+package com.eventi.gestione_eventi;
+
+public class Evento {
+
+    private String name;
+    private Integer seats = 0;
+    private Integer maxSeats;
+
+    public Evento(String name, Integer seats) {
+        if (seats < 0)
+            throw new IllegalArgumentException("Il numero di posti per un evento non può essere negativo");
+        this.seats = seats;
+        this.name = name;
+        this.maxSeats=seats;
+        
+    }
+
+    protected  boolean checkSeatsNumber() {
+        return this.seats>0;
+    }
+
+    @Override
+    public String toString() {
+        return "Evento " + name + ", posti disponibili :" + seats;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+
+
+    protected void addSeats(Integer addedSeats) {
+
+        if (addedSeats < 0)
+            throw new IllegalArgumentException("Non è possibile aggiungere un numero di posti negativo");
+        this.seats += addedSeats;
+        this.maxSeats += addedSeats;
+    }
+
+    
+
+    public void removeSeats(Integer removedSeats) {
+        if (removedSeats < 0)
+            throw new IllegalArgumentException("Non è possibile prenotare  un numero di posti negativo");
+        if(this.seats-removedSeats<0)
+            throw new ArgumentOutOfBoundException("Non ci sono abbastanza posti disponibili");
+
+        this.seats -= removedSeats;
+    }
+
+
+
+}
+
+
